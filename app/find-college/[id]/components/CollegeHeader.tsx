0b74619f3dffc590d4a045c632cb00ca280271d0
Durgesh-Bhandari <durgeshbhandari834@gmail.com>
2026-08-11 17:22:46 +0545

    fix(CollegeHeader): add minimum height to header for better layout consistency

--- app/find-college/[id]/components/CollegeHeader.tsx
+++ app/find-college/[id]/components/CollegeHeader.tsx
@@ -59,7 +59,7 @@ const CollegeHeader: React.FC<CollegeHeaderProps> = ({
   const router = useRouter();
   return (
     <>
-      <div className="relative w-full bg-brand-blue overflow-hidden">
+      <div className="relative w-full bg-brand-blue overflow-hidden min-h-55 md:min-h-[400px]">
         {instBanner && (
           <img
             src={instBanner}
